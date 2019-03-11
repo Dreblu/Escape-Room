b2a5d6a657932c7677a853504d67ea0b3ba32e13
Dreblu <andre_alexander@davincischools.org>
2019-03-11 12:11:46 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -27,6 +27,18 @@ function nextImage2(el){
 		
 	} else if (el.src.match("Images/oxygen.jpeg")){
 		
+		el.src = "Images/orbital.jpeg";
+		
+	} else if (el.src.match("Images/orbital.jpeg")){
+		
+		el.src = "Images/3d.jpg";
+		
+	} else if (el.src.match("Images/3d.jpg")){
+		
+		el.src = "Images/oxygen.jpg";
+		
+	} else if (el.src.match("Images/oxygen.jpg")){
+		
 		el.src = "Images/3d.jpg";
 	
 	} else {
@@ -35,7 +47,7 @@ function nextImage2(el){
 
 //Orbital div
 function nextImage3(el){
-	if (el.src.match("Images/3d.jpg")){
+	if (el.src.match("Images/oxygen.jpg")){
 		
 		el.src = "Images/telo.jpg";
 		
@@ -45,11 +57,11 @@ function nextImage3(el){
 		
 	} else if (el.src.match("Images/orbital.jpeg")){
 		
-		el.src = "Images/oxygen.jpg";
-		
-	} else if (el.src.match("Images/oxygen.jpeg")){
-		
 		el.src = "Images/3d.jpg";
+		
+	} else if (el.src.match("Images/3d.jpg")){
+		
+		el.src = "Images/oxygen.jpg";
 	
 	} else {
 		}
@@ -57,26 +69,40 @@ function nextImage3(el){
 
 //Oxygen div
 function nextImage4(el){
-	if (el.src.match("Images/3d.jpg")){
-		
-		el.src = "Images/telo.jpg";
-		
-	} else if (el.src.match("Images/telo.jpg")){
-		
-		el.src = "Images/orbital.jpeg";
-		
-	} else if (el.src.match("Images/orbital.jpeg")){
+	if (el.src.match("Images/orbital.jpeg")){
 		
 		el.src = "Images/oxygen.jpg";
 		
-	} else if (el.src.match("Images/oxygen.jpeg")){
+	} else if (el.src.match("Images/oxygen.jpg")){
 		
-		el.src = "Images/3d.jpg";
-	
+		el.src = "Images/orbital.jpeg";
+			
 	} else {
 		}
 }
 
-function playSound() {
-	// when the JACKPOT! button is clicked, play the slot machine sound
-}	
+function playsound1(e){
+	document.getElementById("jug").play();
+	document.getElementById("speed").pause();
+	document.getElementById("stamin").pause();
+	document.getElementById("double").pause();
+}
+
+function playsound2(e){
+	document.getElementById("speed").play();
+	document.getElementById("jug").pause();
+	document.getElementById("stamin").pause();
+	document.getElementById("double").pause();
+}
+
+function checkConfiguration(el){
+	if(a.src.match(Images/3d.jpg") && b.src.match(") && c.src.match("") && d.src.match("Images/orbital.jpeg")){
+		a.classList.add("opaque");
+		b.classList.add("opaque");
+		c.classList.add("opaque");
+		d.classList.add("opaque");
+		el.src = "image/money.png";
+		console.log("YOU WIN!");
+	} else {
+		
+	}
